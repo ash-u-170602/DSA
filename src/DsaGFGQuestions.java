26cac4bcfb6b3391dc94dee389a5d43db6e76635
Ashad Naushad <mohda170602@gmail.com>
2023-06-19 13:33:34 +0530

    rearrangeArray

---
+++ src/DsaGFGQuestions.java
@@ -0,0 +1,24 @@
+import java.util.Arrays;
+
+public class DsaGFGQuestions {
+
+    public static void main(String[] args) {
+
+        long[] array = {4, 0, 2, 1, 3};
+        rearrangeArray(array);
+
+    }
+
+    private static void rearrangeArray(long[] array) {
+        System.out.println(Arrays.toString(array));
+
+        for (int i = 0; i < array.length; i++) {
+
+        }
+
+        System.out.println(Arrays.toString(array));
+
+    }
+
+
+}
