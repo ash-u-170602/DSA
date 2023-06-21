f7846176d426444994644634563aa153631b27d0
Ashad Naushad <mohda170602@gmail.com>
2023-06-21 11:03:39 +0530

    Tickets Question

--- src/ArraysHard.java
+++ src/ArraysHard.java
@@ -1,5 +1,4 @@
 import java.util.*;
-
 public class ArraysHard {
 
     public static void main(String[] args) {
--- src/DsaGFGQuestions.java
+++ src/DsaGFGQuestions.java
@@ -1,11 +1,22 @@
+import java.util.ArrayList;
+
 public class DsaGFGQuestions {
 
     public static void main(String[] args) {
 
-        long n = 32;
-        System.out.println(noOfMatchSticks(n));
-
+        int n = 9;
+        int k = 3;
+        System.out.println(distributeTicket(n,k));
     }
+
+    private static int distributeTicket(int n, int k) {
+
+        ArrayList<Integer> queue = new ArrayList<>();
+
+
+        return -1;
+    }
+
 
     private static int noOfMatchSticks(long N) {
         return (N % 5 == 0) ? -1 : (int) (N % 5);
