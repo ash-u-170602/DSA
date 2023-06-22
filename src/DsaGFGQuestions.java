1cef87ef6ef50d03d4616d9b07628c31d01af323
Ashad Naushad <mohda170602@gmail.com>
2023-06-22 11:36:06 +0530

    potd done

--- src/DsaGFGQuestions.java
+++ src/DsaGFGQuestions.java
@@ -4,9 +4,38 @@ public class DsaGFGQuestions {
 
     public static void main(String[] args) {
 
-        int n = 9;
-        int k = 3;
-        System.out.println(distributeTicket(n,k));
+        int[] bills = {5, 5, 10, 10, 20};
+        System.out.println(lemonadeChange(bills));
+    }
+
+    private static boolean lemonadeChange(int[] bills) {
+
+        int count5 = 0;
+        int count10 = 0;
+        int count20 = 0;
+
+        for (int i = 0; i < bills.length; i++) {
+
+            if (bills[i] == 5) {
+                count5++;
+            } else if (bills[i] == 10) {
+                if (count5 > 0) {
+                    count5--;
+                    count10++;
+                } else return false;
+            } else {
+                if (count5 > 3) {
+                    count5 -= 3;
+                    count20++;
+                } else if (count10 > 0 && count5>0) {
+                    count10--;
+                    count5--;
+                    count20++;
+                } else return false;
+            }
+        }
+
+        return true;
     }
 
     private static int distributeTicket(int n, int k) {
